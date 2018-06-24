1f46103a65e343c6061107d46333561fb8c27bea
Jack Baty <jack@fusionary.com>
2018-06-24 08:52:56 -0400

    Build site Sun Jun 24 08:52:56 EDT 2018

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,4 @@
 
 # Called by easy-hugo from emacs
 
-
-echo -e "\033[0;32mDeploying updates to DigitalOcean...\033[0m"
-hugo
-git add -A
-
-msg="rebuilding site `LANG=C date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-git push origin master
-
-#rsync -v -rz -e "ssh -l serverpilot" --checksum --delete --no-perms public/ do.baty.net:/srv/users/serverpilot/apps/batydotnet/public/
+make -i deploy
